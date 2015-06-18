13f0dc2c7742856fd4ac18f0917344207a9ba088
cafedomancer <cafedomancer@gmail.com>
2015-06-18 14:54:24 +0900

    refactor

--- jsonify.py
+++ jsonify.py
@@ -1,79 +1,69 @@
 import bson.json_util
-import json
 import os
-import pprint
 import pymongo
 
 from utils import DATA_DIR
 
 
-def find_pull_request_ids(db, owner, repo, is_merged=True):
-    query = {
-        '$and': [
-            {'owner': owner},
-            {'repo': repo},
-            {'closed_at': {'$ne': None}}
-        ]
-    }
+def find_pull_requests(db, owner=None, repo=None, is_merged=True):
+    query = {'$and': [
+        {'closed_at': {'$ne': None}}
+    ]}
+
+    if owner and repo:
+        query['$and'].append({'owner': owner})
+        query['$and'].append({'repo': repo})
 
     if is_merged:
         query['$and'].append({'merged_at': {'$ne': None}})
     else:
         query['$and'].append({'merged_at': None})
 
-    return [p['number'] for p in db.pull_requests.find(query)]
+    return db.pull_requests.find(query)
 
 
 def find_issue_comments(db, owner, repo, issue_ids):
-    query = {
-        '$and': [
-            {'owner': owner},
-            {'repo': repo},
-            {'issue_id': None}
-        ]
-    }
+    query = {'$and': [
+        {'issue_id': None},
+        {'owner': owner},
+        {'repo': repo}
+    ]}
 
-    all_comments = []
-    for issue_id in issue_ids:
-        query['$and'][2]['issue_id'] = issue_id  # need to be refactored
-        comments = db.issue_comments.find(query)
-        all_comments += comments
+    comments = []
 
-    return all_comments
+    for ii in issue_ids:
+        query['$and'][0].update({'issue_id': ii})
+        comments += db.issue_comments.find(query)
+
+    return comments
 
 
-# prepare database connection
-client = pymongo.MongoClient('localhost', 27017)
-db = client.msr14
+def jsonify_issue_comments(db, owner, repo, is_merged=True):
+    pullreqs = find_pull_requests(db, owner, repo, is_merged)
+    pullreq_ids = [p['number'] for p in pullreqs]
+    comments = find_issue_comments(db, owner, repo, pullreq_ids)
+
+    dirname = os.path.join(DATA_DIR, 'merge' if is_merged else 'unmerge')
+
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    for c in comments:
+        filename = os.path.join(dirname, '{}.json'.format(c['id']))
+        with open(filename, 'w') as f:
+            f.write(bson.json_util.dumps(c) + '\n')
 
 
-# split full name into owner and repo
-full_name = 'rails/rails'
-owner, repo = full_name.split('/')
+if __name__ == '__main__':
+    # prepare database connection
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client.msr14
 
 
-# extract the ids of merged/unmerged pull requests
-merged_ids = find_pull_request_ids(db, owner, repo, is_merged=True)
-unmerged_ids = find_pull_request_ids(db, owner, repo, is_merged=False)
+    # split full name into owner and repo
+    full_name = 'rails/rails'
+    owner, repo = full_name.split('/')
 
-
-# get the issue comments associated with the specific pull request number
-merged_comments = find_issue_comments(db, owner, repo, merged_ids)
-unmerge_comments = find_issue_comments(db, owner, repo, unmerged_ids)
-all_comments = [merged_comments, unmerge_comments]
-
-
-# dump issue comments
-dirs = ['merge', 'unmerge']
-dirs = [os.path.join(DATA_DIR, d) for d in dirs]
-
-for d in dirs:
-    if not os.path.exists(d):
-        os.mkdir(d)
-
-for d, cs in zip(dirs, all_comments):
-    for c in cs:
-        fn = os.path.join(d, '{}.json'.format(c['id']))
-        with open(fn, 'w') as output:
-            output.write(bson.json_util.dumps(c))
-            output.write('\n')
+    # jsonify issue comments
+    jsonify_issue_comments(db, owner, repo, is_merged=True)
+    jsonify_issue_comments(db, owner, repo, is_merged=False)
