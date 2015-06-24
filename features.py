31f8aecba9923b6c1b89911be65ff88e3014afbe
cafedomancer <cafedomancer@gmail.com>
2015-06-24 14:39:00 +0900

    refactor

--- features.py
+++ features.py
@@ -1,9 +1,81 @@
+import re
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def analyze_title(title):
+    tag = re.compile(r'\[(.*?)\]', re.MULTILINE | re.DOTALL)
+    title = tag.sub('', title)
+
+    ruby = re.compile(r'(\w+/)*\w*\.rb', re.MULTILINE | re.DOTALL)
+    title = ruby.sub('', title)
+
+    code = re.compile(r'\w+::\w+(?:::\w+)*(?:\.\w+\??|#\w+\??)?', re.MULTILINE | re.DOTALL)
+    title = code.sub('', title)
+
+    '''
+    issue = re.compile(r'#[0-9]+', re.MULTILINE | re.DOTALL)
+    title = issue.sub('', title)
+
+    url = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.MULTILINE | re.DOTALL)
+    title = url.sub('', title)
+    '''
+
+    vectorizer = CountVectorizer(stop_words='english')
+    analyzer = vectorizer.build_analyzer()
+
+    title = analyzer(title)
+
+    title = list(filter(lambda s: not '_' in s, title))
+    title = list(filter(lambda s: not s.isdigit(), title))
+
+    return title
+
+
+def analyze_body(body):
+    pass
+
+
+def analyze_comment(comment):
+    pass
+
+
+if __name__ == '__main__':
+    import pymongo
+
+    from collections import defaultdict
+    from operator import itemgetter
+    from pprint import pprint
+    from utils import find_pull_requests
+
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client.msr14
+
+    full_name = 'rails/rails'
+    owner, repo = full_name.split('/')
+
+    pullreqs = find_pull_requests(db, owner, repo, is_merged=True)
+    titles = [p['title'] for p in pullreqs]
+
+    titles = list(map(analyze_title, titles))
+
+    usage = defaultdict(int)
+    for t in titles:
+        for w in t:
+            usage[w] += 1
+
+    pprint(sorted(usage.items(), key=itemgetter(1), reverse=True))
+
+
+
+'''
 import bson.json_util
 import glob
 import misaka
 import numpy as np
 import os
 import pprint
+import random
 import re
 
 from bs4 import BeautifulSoup
@@ -13,44 +85,93 @@ from utils import DATA_DIR
 
 
 # load JSON files of issue comments
-merge_dir = os.path.join(DATA_DIR, 'unmerge')
+merge_dir = os.path.join(DATA_DIR, 'merge')
 merge_glob = glob.glob('{}/*.json'.format(merge_dir))
 
+taints = ['/Users/cafedomancer/Desktop/thesis/data/merge/1115656.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/13427639.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/20442668.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/24924043.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/6049037.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/6384150.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/6603791.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/6604355.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/6604472.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/6874873.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/6984582.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/7287851.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/7642324.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/8301966.json',
+          '/Users/cafedomancer/Desktop/thesis/data/merge/8757750.json']
+
+for t in taints:
+    merge_glob.remove(t)
+
+unmerge_dir = os.path.join(DATA_DIR, 'unmerge')
+unmerge_glob = glob.glob('{}/*.json'.format(unmerge_dir))
+
 comments = []
-for fn in merge_glob[:5000]:
+for fn in merge_glob:
     with open(fn) as f:
-        comments.append(bson.json_util.loads(f.read()))
+        comment = bson.json_util.loads(f.read())
+        comment = comment['body']
+        comment = comment.replace('\r\n', '\n')
+        comments.append(comment)
 
 
-# extract body text
-comments = [c['body'] for c in comments]
-# get rid of carriage return
-comments = [c.replace('\r\n', '\n') for c in comments]
-# retrieve email replay
+raws = comments
+
+
+# remove code blocks
+code_pattern = re.compile('^```(.*?)```$', re.MULTILINE | re.DOTALL)
+comments = [code_pattern.sub('', c) for c in comments]
+
+# remove inline codes
+inline_code_pattern = re.compile('`(.*?)`', re.MULTILINE | re.DOTALL)
+comments = [inline_code_pattern.sub('', c) for c in comments]
+
+# remove email replay
 comments = [EmailReplyParser.parse_reply(c) for c in comments]
-# convert markdown to html
-comments = [misaka.html(c) for c in comments]
-# convert html to plain text
-comments = [BeautifulSoup(c).get_text() for c in comments]
+
+# remove emojis
+emoji_pattern = re.compile(':(.*?)heart:', re.MULTILINE | re.DOTALL)
+comments = [emoji_pattern.sub('', c) for c in comments]
+
 # remove URLs
-comments = [re.sub('(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?', '', c) for c in comments]
+url_pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.MULTILINE | re.DOTALL)
+comments = [url_pattern.sub('', c) for c in comments]
 
 
-# random.shuffle(comments)
-# comments = sorted(comments, key=len, reverse=True)
-# comments = comments[:1000]
-# for i, c in enumerate(comments):
-#     if '```' in c and '(http' in c:
-#         print(i)
-#         print(c)
-#         print('-'*100)
+# test substitution
+zips = list(zip(merge_glob, raws, comments))
+for g, r, c in zips:
+    if emoji_pattern.search(r):
+        # print(g)
+        # print('=' * 80)
+        print(r)
+        print('-' * 80)
+        print(c)
+        print('=' * 80)
 
 
-vectorizer = CountVectorizer(min_df=2, ngram_range=(4, 4), stop_words='english')
+vectorizer = CountVectorizer(min_df=10, ngram_range=(3, 3), stop_words='english')
 vectorized = vectorizer.fit_transform(comments).toarray()
-
 freq = []
 for term, index in vectorizer.vocabulary_.items():
     freq.append((term, np.sum(vectorized[:, index])))
-
 pprint.pprint(sorted(freq, key=lambda x: x[1], reverse=True))
+
+
+# extract body text
+# comments = [c['body'] for c in comments]
+# get rid of carriage return
+# comments = [c.replace('\r\n', '\n') for c in comments]
+# retrieve email replay
+# comments = [EmailReplyParser.parse_reply(c) for c in comments]
+# convert markdown to html
+# comments = [misaka.html(c) for c in comments]
+# convert html to plain text
+# comments = [BeautifulSoup(c).get_text() for c in comments]
+# remove URLs
+# comments = [re.sub('(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?', '', c) for c in comments]
+'''
--- hdp-lda/01_title.py
+++ hdp-lda/01_title.py
@@ -1,83 +1,53 @@
 import pymongo
-import re
+import sys
+
 from collections import defaultdict
 from gensim import corpora, models, similarities
-from sklearn.feature_extraction.text import CountVectorizer
+from operator import itemgetter
+from pprint import pprint
+
+sys.path.append('..')
+
+from features import analyze_title
+from utils import find_pull_requests
 
 
-def find_pull_requests(db, owner=None, repo=None, is_merged=True):
-    query = {'$and': [
-        {'closed_at': {'$ne': None}}
-    ]}
+# prepare database connection
+client = pymongo.MongoClient('localhost', 27017)
+db = client.msr14
 
-    if owner and repo:
-        query['$and'].append({'owner': owner})
-        query['$and'].append({'repo': repo})
+# split full name into owner and repo
+full_name = 'rails/rails'
+owner, repo = full_name.split('/')
 
-    if is_merged:
-        query['$and'].append({'merged_at': {'$ne': None}})
-    else:
-        query['$and'].append({'merged_at': None})
+# extract pull request titles
+pullreqs = find_pull_requests(db, owner, repo, is_merged=False)
+titles = [p['title'] for p in pullreqs]
 
-    return db.pull_requests.find(query)
+# analyze pull request titles
+titles = list(map(analyze_title, titles))
 
+# count frequency
+usage = defaultdict(int)
+for t in titles:
+    for w in t:
+        usage[w] += 1
 
-def analyze_pull_request_title(title):
-    tag = re.compile(r'\[(.*?)\]', re.MULTILINE | re.DOTALL)
-    title = tag.sub('', title)
+# remove words that only appear once
+titles = [[w for w in t if usage[w] > 1] for t in titles]
 
-    code = re.compile(r'\w*::\w*[.#]*\w*\.?\(?\w*\)?\??', re.MULTILINE | re.DOTALL)
-    title = code.sub('', title)
+print(len(titles))
+print(sum(len(t) for t in titles))
 
-    code = re.compile(r'\w*#\w*', re.MULTILINE | re.DOTALL)
-    title = code.sub('', title)
+# create topic model
+dictionary = corpora.Dictionary(titles)
+corpus = [dictionary.doc2bow(t) for t in titles]
+model = models.hdpmodel.HdpModel(corpus, id2word=dictionary)
 
-    issue = re.compile(r'#[0-9]+', re.MULTILINE | re.DOTALL)
-    title = issue.sub('', title)
-
-    url = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', re.MULTILINE | re.DOTALL)
-    title = url.sub('', title)
-
-    vectorizer = CountVectorizer(stop_words='english')
-    analyzer = vectorizer.build_analyzer()
-
-    title = analyzer(title)
-
-    title = list(filter(lambda s: not '_' in s, title))
-    title = list(filter(lambda s: not s.isdigit(), title))
-
-    return title
-
-
-if __name__ == '__main__':
-    # prepare database connection
-    client = pymongo.MongoClient('localhost', 27017)
-    db = client.msr14
-
-    # split full name into owner and repo
-    full_name = 'rails/rails'
-    owner, repo = full_name.split('/')
-
-    # extract pull request titles
-    pullreqs = find_pull_requests(db, owner, repo, is_merged=False)
-    titles = [p['title'] for p in pullreqs]
-
-    # analyze pull request titles
-    titles = list(map(analyze_pull_request_title, titles))
-
-    # count frequency
-    usage = defaultdict(int)
-    for t in titles:
-        for w in t:
-            usage[w] += 1
-
-    # remove words that only appear once
-    titles = [[w for w in t if usage[w] > 1] for t in titles]
-
-    # create topic model
-    dictionary = corpora.Dictionary(titles)
-    corpus = [dictionary.doc2bow(t) for t in titles]
-    model = models.hdpmodel.HdpModel(corpus, id2word=dictionary)
-
-    import pprint
-    pprint.pprint(model.show_topics(formatted=False))
+# print topics
+# pprint(model.show_topics(formatted=False))
+for topic in model.show_topics(topics=10, topn=10, formatted=False):
+    topic = itemgetter(1)(topic)
+    topic = list(map(itemgetter(0), topic))
+    topic = ' '.join(topic)
+    print(topic)
