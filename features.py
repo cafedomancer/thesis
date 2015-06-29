3098d3cd94c30fe15c0a3e9289b2ad98defc6433
cafedomancer <cafedomancer@gmail.com>
2015-06-29 15:04:30 +0900

    clean and analyze issue comments

--- features.py
+++ features.py
@@ -1,5 +1,6 @@
 import re
 
+from email_reply_parser import EmailReplyParser
 from sklearn.feature_extraction.text import CountVectorizer
 
 
@@ -62,8 +63,43 @@ def analyze_body(body):
     return body
 
 
+def clean_comment(comment):
+    comment = EmailReplyParser.parse_reply(comment)
+
+    block = re.compile(r'`{3,}.*?`{3,}|<pre>.*?</pre>|^(?: {4,}|\t+).*?$', re.MULTILINE | re.DOTALL)
+    comment = block.sub('', comment)
+
+    inline = re.compile(r'`.*?`', re.MULTILINE | re.DOTALL)
+    comment = inline.sub('', comment)
+
+    link = re.compile(r'!?\[(.*?)\]\(.*?\)', re.MULTILINE | re.DOTALL)
+    comment = link.sub(r'\1', comment)
+
+    url = re.compile(r'\(?https?://\S+\)?', re.MULTILINE | re.DOTALL)
+    comment = url.sub('', comment)
+
+    code = re.compile(r'(?:[A-Z][a-z]*){2,}(?:::(?:[A-Z][a-z]*)+)*(?:\.|#\S+)*', re.MULTILINE | re.DOTALL)
+    comment = code.sub('', comment)
+
+    ruby = re.compile(r'(?:\w+/)*\w*\.rb', re.MULTILINE | re.DOTALL)
+    comment = ruby.sub('', comment)
+
+    emoji = re.compile(r':\S+:', re.MULTILINE | re.DOTALL)
+    comment = emoji.sub('', comment)
+
+    return comment
+
+
 def analyze_comment(comment):
-    pass
+    vectorizer = CountVectorizer(stop_words='english')
+    analyzer = vectorizer.build_analyzer()
+
+    comment = analyzer(clean_comment(comment))
+
+    comment = list(filter(lambda s: not '_' in s, comment))
+    comment = list(filter(lambda s: not any(c.isdigit() for c in s), comment))
+
+    return comment
 
 
 if __name__ == '__main__':
@@ -72,7 +108,7 @@ if __name__ == '__main__':
     from collections import defaultdict
     from operator import itemgetter
     from pprint import pprint
-    from utils import find_pull_requests
+    from utils import find_pull_requests, load_issue_comments
 
     client = pymongo.MongoClient('localhost', 27017)
     db = client.msr14
@@ -80,7 +116,8 @@ if __name__ == '__main__':
     full_name = 'rails/rails'
     owner, repo = full_name.split('/')
 
-    pullreqs = find_pull_requests(db, owner, repo, is_merged=True)
+    # pullreqs = find_pull_requests(db, owner, repo, is_merged=True)
+    comments = load_issue_comments(is_merged=True)
 
     usage = defaultdict(int)
 
@@ -90,11 +127,16 @@ if __name__ == '__main__':
     #     for w in t:
     #         usage[w] += 1
 
-    bodies = [p['body'] for p in pullreqs]
-    bodies = list(filter(bool, bodies))
-    bodies = list(map(analyze_body, bodies))
-    for b in bodies:
-        for w in b:
+    # bodies = [p['body'] for p in pullreqs]
+    # bodies = list(filter(bool, bodies))
+    # bodies = list(map(analyze_body, bodies))
+    # for b in bodies:
+    #     for w in b:
+    #         usage[w] += 1
+
+    comments = list(map(analyze_comment, comments))
+    for c in comments:
+        for w in c:
             usage[w] += 1
 
     pprint(sorted(usage.items(), key=itemgetter(1), reverse=True))
